38e3ae1a5ccc61b2387e50b67d480b0462eba466
Inzamam Malik <malikasinger1@gmail.com>
2016-04-01 08:38:34 +0500

    company feature added

---
+++ Server/static/adminPortal/js/app.js
@@ -0,0 +1,62 @@
+angular.module('starter', ['ionic'])
+  .config(function($stateProvider, $urlRouterProvider,$httpProvider ){
+    
+    
+    $stateProvider
+      .state("login", {
+        url : "/login",
+        templateUrl : "/adminPortal/templates/login.html",
+        controller  : "loginCtrl" 
+      })
+      .state("signup", {
+        url : "/signup",
+        templateUrl : "/adminPortal/templates/signup.html",
+        controller  : "signupCtrl" 
+      })
+      .state("home", {
+        url : "/",
+        templateUrl : "/adminPortal/templates/home.html",
+        controller  : "homeCtrl",
+        loginCompulsory : true
+      })
+        .state("home.new", {
+        url : "",
+        templateUrl : "/adminPortal/templates/new-company.html",
+        controller  : "new-companyCtrl"
+      })
+        .state("home.view", {
+        url : "",
+        templateUrl : "/adminPortal/templates/view-company.html",
+        controller  : "view-companyCtrl"
+      });
+      
+      
+      $urlRouterProvider.otherwise("/");
+      
+      
+      $httpProvider.interceptors.push('httpInterceptor');
+  })
+  .run(function($rootScope, $state){
+    
+    $rootScope.$on("$stateChangeStart", function(event, toState){
+      var firebaseLocalToken = localStorage.getItem("token");
+        
+      if(toState.loginCompulsory && !firebaseLocalToken){ 
+        event.preventDefault();
+        $state.go("login");
+      }
+        
+    });
+    
+  })
+  .factory("httpInterceptor", function(){
+    return {
+      request : function(config){
+        var token = localStorage.getItem("token");
+        if(token){
+          config.url = config.url + "?token="+token;
+        }
+        return config;
+      }
+    }       
+  });
